1787039804d81a2f1b8b9943d6effaa13c657548
RSkala <RichardSkala@gmail.com>
2024-06-27 15:21:14 -0700

    Added ZombieGameModeBase C++ backing class

---
+++ Source/ZombieTopDownGame1/Public/Game/ZombieGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ZombieGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ZOMBIETOPDOWNGAME1_API AZombieGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
